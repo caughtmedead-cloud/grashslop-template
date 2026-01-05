87e95ec2e6a1b4f9bbdfd63e972229a398015f9d
caughtmedead-cloud <caughtmedead@gmail.com>
2026-01-05 01:35:47 -0500

    Finally have animation system working plus good commit if this doesn't work im gonna commit sudoku

--- Assets/_Scripts/Player/Animation/PlayerAnimator.cs
+++ Assets/_Scripts/Player/Animation/PlayerAnimator.cs
@@ -43,7 +43,7 @@ public class PlayerAnimator : NetworkBehaviour
         playerController = GetComponent<PlayerController>();
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         if (!IsOwner) return;
 
